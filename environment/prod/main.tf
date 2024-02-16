b6cf44201c56293a6c628d77ba8f3255f150eece
Luke Appleton <luke.d.appleton@gmail.com>
2024-02-15 20:54:30 -0700

    Temp commented out the version

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,12 +1,12 @@
 module "provisioning" {
-    source      = "git@github.com:lukedappleton/gcloud-tf.git//modules/provisioning?ref=75443f7"
+    source      = "git@github.com:lukedappleton/gcloud-tf.git//modules/provisioning?ref=4edaeb0"
     environment = var.environment
     project_id  = var.project_id
     region      = var.region
 }
 
 module "gke_cluster" {
-  source            = "git@github.com:lukedappleton/gcloud-tf.git//modules/k8s?ref=75443f7"
+  source            = "git@github.com:lukedappleton/gcloud-tf.git//modules/k8s?ref=4edaeb0"
   project_id        = var.project_id
   region            = var.region
   environment       = var.environment
